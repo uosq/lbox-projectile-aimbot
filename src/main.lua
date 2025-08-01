1da26470eabdf421a25ef8c99c6f2997533cb391
uosq <lboxwallytf2@gmail.com>
2025-07-31 23:37:31 -0300

    fix other weapons and fix projectile path

--- src/main.lua
+++ src/main.lua
@@ -120,7 +120,6 @@ local paths = {
 }
 
 local original_gui_value = gui.GetValue("projectile aimbot")
-local original_auto_reload = tostring(client.GetConVar("cl_autoreload"))
 
 local function CanRun(pLocal, pWeapon, bIsBeggar, bIgnoreKey)
 	if clientstate:GetChokedCommands() > 0 then
@@ -371,232 +370,100 @@ local function GetCharge(pWeapon)
 	return charge_time
 end
 
-----@param uCmd UserCmd
---[[local function CreateMove(uCmd)
-	if not settings.enabled then
-		return
-	end
-
-	local netchannel = clientstate.GetNetChannel()
-	if not netchannel then
-		return
-	end
-
-	local pLocal = entities.GetLocalPlayer()
-	if pLocal == nil then
-		return
-	end
-
-	local pWeapon = pLocal:GetPropEntity("m_hActiveWeapon")
-	if pWeapon == nil then
-		return
-	end
-
-	local bIsBeggar = pWeapon:GetPropInt("m_iItemDefinitionIndex") == BEGGARS_BAZOOKA_INDEX
-
-	if not CanRun(pLocal, pWeapon, bIsBeggar, false) then
-		return
-	end
-
-	if gui.GetValue("projectile aimbot") ~= "none" then
-		gui.SetValue("projectile aimbot", "none")
-	end
-
-	--- fuck you psilent
-	--[[if tostring(client.GetConVar("cl_autoreload")) == "1" then
-		client.SetConVar("cl_autoreload", "0")
-	end
-
-	local iWeaponID = pWeapon:GetWeaponID()
-	local bAimAtTeamMates = false
-	local bIsSandvich = false
-
-	if iWeaponID == E_WeaponBaseID.TF_WEAPON_LUNCHBOX then
-		bAimAtTeamMates = true
-		bIsSandvich = true
-	elseif iWeaponID == E_WeaponBaseID.TF_WEAPON_CROSSBOW then
-		bAimAtTeamMates = true
-	end
-
-	bAimAtTeamMates = settings.allow_aim_at_teammates and bAimAtTeamMates or false
-
-	local weaponInfo = GetProjectileInformation(pWeapon:GetPropInt("m_iItemDefinitionIndex"))
-	local vecHeadPos = pLocal:GetAbsOrigin() + pLocal:GetPropVector("localdata", "m_vecViewOffset[0]")
-	-- real charge for bows / stickies / Beggar etc.
-	local charge_time = GetCharge(pWeapon)
-
-	local players = entities.FindByClass("CTFPlayer")
-
-	local best_target = GetClosestEntityToFov(pLocal, vecHeadPos, players, bAimAtTeamMates)
-	if not best_target.index then
-		return nil
-	end
-
-	local pTarget = entities.GetByIndex(best_target.index)
-	if not pTarget then
-		return nil
-	end
-
-	local nlatency = settings.ping_compensation and 0
-		or netchannel:GetLatency(E_Flows.FLOW_OUTGOING) + netchannel:GetLatency(E_Flows.FLOW_INCOMING)
-	local flStepSize = pTarget:GetPropFloat("m_flStepSize")
-
-	local vecTargetOrigin = pTarget:GetAbsOrigin()
-
-	local vecWeaponFirePos =
-		weaponInfo:GetFirePosition(pLocal, vecHeadPos, engine.GetViewAngles(), pWeapon:IsViewModelFlipped())
-
-	local dist = (vecHeadPos - vecTargetOrigin):Length()
-	if dist > settings.max_distance then
-		return nil
-	end
-
-	local velocity_vector = weaponInfo:GetVelocity(charge_time) -- use real charge
-	local forward_speed = math.sqrt(velocity_vector.x ^ 2 + velocity_vector.y ^ 2)
-
-	local det_mult = pWeapon:AttributeHookFloat("sticky_arm_time")
-	local detonate_time = pWeapon:GetWeaponID() == E_WeaponBaseID.TF_WEAPON_PIPEBOMBLAUNCHER and 0.7 * det_mult or 0
-	local travel_time_est = (vecTargetOrigin - vecHeadPos):Length() / forward_speed
-	local choked_time = (clientstate:GetChokedCommands() / 66) * 1000
-	local total_time = travel_time_est + nlatency + detonate_time + choked_time
-
-	if total_time > settings.max_sim_time or total_time > weaponInfo.m_flLifetime then
-		return nil
-	end
-
-	local time_ticks = (((total_time * 66.67) + 0.5) // 1)
-
-	local player_positions = player_sim.Run(flStepSize, pTarget, vecTargetOrigin, time_ticks)
-	if not player_positions then
-		return nil
-	end
-
-	local predicted_target_pos = player_positions[#player_positions] or vecTargetOrigin
-
-	-- Make traces ignore *us* and also the target's **current** position,
-	-- because we're aiming at where he *will* be, not where he is now.
-	local function shouldHit(ent)
-		if not ent then -- world / sky / nil
-			return true -- trace should go on
+---@param uCmd UserCmd
+---@param pWeapon Entity
+---@param pLocal Entity
+---@param angle EulerAngles
+---@param player_path table<integer, Vector3>
+---@param vecHeadPos Vector3
+---@param total_time number
+---@param weaponInfo WeaponInfo
+---@param charge number
+local function HandleWeaponFiring(uCmd, pLocal, pWeapon, angle, player_path, vecHeadPos, weaponInfo, total_time, charge)
+	if pWeapon:GetWeaponID() == E_WeaponBaseID.TF_WEAPON_COMPOUND_BOW then
+		if charge > 0 then
+			if settings.autoshoot and wep_utils.CanShoot() then
+				uCmd.buttons = uCmd.buttons | IN_ATTACK
+			end
+			if (uCmd.buttons & IN_ATTACK) ~= 0 then
+				uCmd.buttons = uCmd.buttons & ~IN_ATTACK -- release to fire
+				if settings.psilent then
+					uCmd.sendpacket = false
+				end
+				uCmd.viewangles = Vector3(angle:Unpack())
+				displayed_time = globals.CurTime() + settings.draw_time
+				paths.player_path = player_path
+				paths.proj_path = proj_sim.Run(pLocal, pWeapon, vecHeadPos, angle:Forward(), total_time, weaponInfo)
+			end
+		else
+			if settings.autoshoot and wep_utils.CanShoot() then
+				uCmd.buttons = uCmd.buttons | IN_ATTACK -- hold to charge
+			end
 		end
-		if ent == pLocal or ent == pTarget then
-			return false -- pretend they don't exist
-		end
-		return ent:GetTeamNumber() ~= pTarget:GetTeamNumber()
-	end
-
-	local vecMins, vecMaxs = weaponInfo.m_vecMins, weaponInfo.m_vecMaxs
-	local trace = engine.TraceHull(vecWeaponFirePos, predicted_target_pos, vecMins, vecMaxs, MASK_SHOT_HULL, shouldHit)
-	local is_visible = trace and (trace.fraction >= 0.9 or trace.entity == pTarget)
-
-	local bIsHuntsman = pWeapon:GetWeaponID() == E_WeaponBaseID.TF_WEAPON_COMPOUND_BOW
-
-	if (not is_visible or bIsHuntsman) and settings.multipointing then
-		local bSplashWeapon = pWeapon:GetWeaponProjectileType() == E_ProjectileType.TF_PROJECTILE_ROCKET
-			or pWeapon:GetWeaponProjectileType() == E_ProjectileType.TF_PROJECTILE_PIPEBOMB_REMOTE
-			or pWeapon:GetWeaponProjectileType() == E_ProjectileType.TF_PROJECTILE_PIPEBOMB_PRACTICE
-			or pWeapon:GetWeaponProjectileType() == E_ProjectileType.TF_PROJECTILE_CANNONBALL
-
-		multipoint:Set(
-			pLocal,
-			pWeapon,
-			pTarget,
-			bIsHuntsman,
-			bAimAtTeamMates,
-			vecHeadPos,
-			predicted_target_pos,
-			weaponInfo,
-			math_utils,
-			settings.max_distance,
-			bSplashWeapon,
-			ent_utils,
-			settings
-		)
-
-		local best_multipoint = multipoint:GetBestHitPoint()
-		if not best_multipoint then
-			return
-		end
-
-		predicted_target_pos = best_multipoint
-	end
-
-	-- Recheck trace for final prediction
-	trace = engine.TraceHull(vecWeaponFirePos, predicted_target_pos, vecMins, vecMaxs, MASK_SHOT_HULL, shouldHit)
-	if not trace or (trace.fraction < 0.9 and trace.entity ~= pTarget) then
-		return
-	end
-
-	local gravity = client.GetConVar("sv_gravity") * weaponInfo:GetGravity(charge_time)
-	local angle = math_utils.SolveBallisticArc(vecHeadPos, predicted_target_pos, forward_speed, gravity)
-	if not angle then
-		return
-	end
-
-	local bAttack = false
-	local bIsStickybombLauncher = pWeapon:GetWeaponID() == E_WeaponBaseID.TF_WEAPON_PIPEBOMBLAUNCHER
-
-	if bIsBeggar then
+	elseif pWeapon:GetPropInt("m_iItemDefinitionIndex") == BEGGARS_BAZOOKA_INDEX then
 		local clip = pWeapon:GetPropInt("LocalWeaponData", "m_iClip1")
 		if clip < 1 then
 			uCmd.buttons = uCmd.buttons | IN_ATTACK -- hold to charge
 		else
 			uCmd.buttons = uCmd.buttons & ~IN_ATTACK -- release to fire
-			bAttack = true
+			if settings.psilent then
+				uCmd.sendpacket = false
+			end
+			uCmd.viewangles = Vector3(angle:Unpack())
+			displayed_time = globals.CurTime() + settings.draw_time
+			paths.player_path = player_path
+			paths.proj_path = proj_sim.Run(pLocal, pWeapon, vecHeadPos, angle:Forward(), total_time, weaponInfo)
 		end
-	elseif bIsHuntsman then
-		if charge_time > 0.0 then
-			if settings.autoshoot and wep_utils.CanShoot() then
-				uCmd.buttons = uCmd.buttons | IN_ATTACK
-			end
-
-			if (uCmd.buttons & IN_ATTACK) ~= 0 then
-				uCmd.buttons = uCmd.buttons & ~IN_ATTACK -- release to fire
-				bAttack = true
-			end
-		else
-			if settings.autoshoot then
-				uCmd.buttons = uCmd.buttons | IN_ATTACK -- hold to charge
-			end
-		end
-	elseif bIsStickybombLauncher then
+	elseif pWeapon:GetWeaponID() == E_WeaponBaseID.TF_WEAPON_PIPEBOMBLAUNCHER then
 		if settings.autoshoot and wep_utils.CanShoot() then
 			uCmd.buttons = uCmd.buttons | IN_ATTACK
 		end
 
-		if charge_time > 0.0 then
+		if charge > 0 then
 			uCmd.buttons = uCmd.buttons & ~IN_ATTACK -- release to fire
-			bAttack = true
+			if settings.psilent then
+				uCmd.sendpacket = false
+			end
+			uCmd.viewangles = Vector3(angle:Unpack())
+			displayed_time = globals.CurTime() + settings.draw_time
+			paths.player_path = player_path
+			paths.proj_path = proj_sim.Run(pLocal, pWeapon, vecHeadPos, angle:Forward(), total_time, weaponInfo)
 		end
-	elseif bIsSandvich then
+	elseif pWeapon:GetWeaponID() == E_WeaponBaseID.TF_WEAPON_LUNCHBOX then
 		uCmd.buttons = uCmd.buttons | IN_ATTACK2
-		bAttack = true -- special case for sandvich
-	else -- generic weapons
-		if settings.autoshoot then
-			uCmd.buttons = uCmd.buttons | IN_ATTACK
-		end
-
-		bAttack = (uCmd.buttons & IN_ATTACK) ~= 0
-	end
-
-	if bAttack then
-		local m_iReloadMode = pWeapon:GetPropInt("m_iReloadMode")
-		local can_psilent = not bIsSandvich and settings.psilent-- and m_iReloadMode == 0
-
-		if can_psilent then
-			uCmd.sendpacket = false
-		end
-
-		uCmd:SetViewAngles(angle:Unpack())
+		uCmd.viewangles = Vector3(angle:Unpack())
 		displayed_time = globals.CurTime() + settings.draw_time
-		paths.player_path = player_positions
-		paths.proj_path = proj_sim.Run(pLocal, pWeapon, vecWeaponFirePos, angle:Forward(), total_time, weaponInfo)
-		return
+		paths.player_path = player_path
+		paths.proj_path = proj_sim.Run(pLocal, pWeapon, vecHeadPos, angle:Forward(), total_time, weaponInfo)
+	else
+		if wep_utils.CanShoot() then
+			if settings.autoshoot and (uCmd.buttons & IN_ATTACK) == 0 then
+				uCmd.buttons = uCmd.buttons | IN_ATTACK
+			end
+
+			if (uCmd.buttons & IN_ATTACK) ~= 0 then
+				if settings.psilent then
+					uCmd.sendpacket = false
+				end
+				uCmd.viewangles = Vector3(angle:Unpack())
+				displayed_time = globals.CurTime() + settings.draw_time
+				paths.player_path = player_path
+				paths.proj_path = proj_sim.Run(pLocal, pWeapon, vecHeadPos, angle:Forward(), total_time, weaponInfo)
+			end
+		end
 	end
-end]]
+end
 
 ---@param uCmd UserCmd
 local function CreateMove(uCmd)
+	if settings.enabled == false then
+		return
+	end
+
+	if (engine.IsChatOpen() or engine.Con_IsVisible() or engine.IsGameUIVisible()) == true then
+		return false
+	end
+
 	if input.IsButtonDown(gui.GetValue("aim key")) == false then
 		return
 	end
@@ -606,14 +473,37 @@ local function CreateMove(uCmd)
 		return
 	end
 
+	if pLocal:InCond(E_TFCOND.TFCond_Taunting) then
+		return
+	end
+
+	if pLocal:InCond(E_TFCOND.TFCond_HalloweenKart) then
+		return
+	end
+
 	local pWeapon = pLocal:GetPropEntity("m_hActiveWeapon")
 	if pWeapon == nil then
 		return
 	end
 
+	if pWeapon:IsMeleeWeapon() then
+		return false
+	end
+
+	local iWeaponID = pWeapon:GetWeaponID()
+	local bAimAtTeamMates = false
+
+	if iWeaponID == E_WeaponBaseID.TF_WEAPON_LUNCHBOX then
+		bAimAtTeamMates = true
+	elseif iWeaponID == E_WeaponBaseID.TF_WEAPON_CROSSBOW then
+		bAimAtTeamMates = true
+	end
+
+	bAimAtTeamMates = settings.allow_aim_at_teammates and bAimAtTeamMates or false
+
 	local players = entities.FindByClass("CTFPlayer")
 	local vecHeadPos = pLocal:GetAbsOrigin() + pLocal:GetPropVector("localdata", "m_vecViewOffset[0]")
-	local pTargetInfo = GetClosestEntityToFov(pLocal, vecHeadPos, players, false)
+	local pTargetInfo = GetClosestEntityToFov(pLocal, vecHeadPos, players, bAimAtTeamMates)
 	if pTargetInfo == nil then
 		return
 	end
@@ -661,18 +551,6 @@ local function CreateMove(uCmd)
 	local is_visible = trace and (trace.fraction >= 0.9 or trace.entity == pTarget)
 
 	local bIsHuntsman = pWeapon:GetWeaponID() == E_WeaponBaseID.TF_WEAPON_COMPOUND_BOW
-	local iWeaponID = pWeapon:GetWeaponID()
-	local bAimAtTeamMates = false
-	local bIsSandvich = false
-
-	if iWeaponID == E_WeaponBaseID.TF_WEAPON_LUNCHBOX then
-		bAimAtTeamMates = true
-		bIsSandvich = true
-	elseif iWeaponID == E_WeaponBaseID.TF_WEAPON_CROSSBOW then
-		bAimAtTeamMates = true
-	end
-
-	bAimAtTeamMates = settings.allow_aim_at_teammates and bAimAtTeamMates or false
 
 	if (not is_visible or bIsHuntsman) and settings.multipointing then
 		local bSplashWeapon = pWeapon:GetWeaponProjectileType() == E_ProjectileType.TF_PROJECTILE_ROCKET
@@ -704,32 +582,19 @@ local function CreateMove(uCmd)
 		vecPredictedPos = best_multipoint
 	end
 
-	trace = engine.TraceHull(vecHeadPos, vecPredictedPos, vecMins, vecMaxs, MASK_SHOT_HULL, shouldHit)
-	if not trace or (trace.fraction < 0.9 and trace.entity ~= pTarget) then
-		return
-	end
-
 	local gravity = client.GetConVar("sv_gravity") * weaponInfo:GetGravity(charge_time)
 	local angle = math_utils.SolveBallisticArc(vecHeadPos, vecPredictedPos, forward_speed, gravity)
 	if angle == nil then
 		return
 	end
 
-	if wep_utils.CanShoot() then
-		if settings.autoshoot and (uCmd.buttons & IN_ATTACK) == 0 then
-			uCmd.buttons = uCmd.buttons | IN_ATTACK
-		end
-
-		if (uCmd.buttons & IN_ATTACK) ~= 0 then
-			if settings.psilent then
-				uCmd.sendpacket = false
-			end
-			uCmd.viewangles = Vector3(angle:Unpack())
-			displayed_time = globals.CurTime() + settings.draw_time
-			paths.player_path = player_path
-			paths.proj_path = proj_sim.Run(pLocal, pWeapon, vecHeadPos, angle:Forward(), total_time, weaponInfo)
-		end
+	local vecWeaponFirePos = weaponInfo:GetFirePosition(pLocal, vecHeadPos, angle, pWeapon:IsViewModelFlipped())
+	trace = engine.TraceHull(vecWeaponFirePos, vecPredictedPos, vecMins, vecMaxs, MASK_SHOT_HULL, shouldHit)
+	if not trace or (trace.fraction < 0.9 and trace.entity ~= pTarget) then
+		return
 	end
+
+	HandleWeaponFiring(uCmd, pLocal, pWeapon, angle, player_path, vecWeaponFirePos, weaponInfo, total_time, charge_time)
 end
 
 --- Terminator (titaniummachine1) made this
@@ -873,7 +738,7 @@ local function Unload()
 	proj_sim = nil
 
 	gui.SetValue("projectile aimbot", original_gui_value)
-	client.SetConVar("cl_autoreload", original_auto_reload)
+	--client.SetConVar("cl_autoreload", original_auto_reload)
 end
 
 callbacks.Register("CreateMove", "ProjAimbot CreateMove", CreateMove)
