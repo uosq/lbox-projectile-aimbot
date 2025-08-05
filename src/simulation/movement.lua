786ca42c3e183283c97cb43a90060811697ad583
uosq <lboxwallytf2@gmail.com>
2025-08-05 16:31:59 -0300

    add some stuff

--- src/simulation/movement.lua
+++ src/simulation/movement.lua
@@ -15,7 +15,7 @@ local GameMovement = {
     m_iWaterLevel = nil,
     m_flFallVelocity = nil,
     m_iSpeedCropped = nil,
-    m_flMaxspeed = nil
+    m_flMaxspeed = nil,
 };
 
 --- i think not even NASA uses a pi this big wtf
@@ -61,12 +61,11 @@ function GameMovement:CheckWater()
 
     local vPlayerMins = self.m_hTarget:GetMins();
     local vPlayerMaxs = self.m_hTarget:GetMaxs();
-    local vTargetOrigin = self.m_hTarget:GetAbsOrigin()
 
     point = Vector3()
-    point.x = vTargetOrigin.x + (vPlayerMins.x + vPlayerMaxs.x) * 0.5;
-    point.y = vTargetOrigin.y + (vPlayerMins.y + vPlayerMaxs.y) * 0.5;
-    point.z = vTargetOrigin.z + vPlayerMins.z + 1
+    point.x = self.m_vecPos.x + (vPlayerMins.x + vPlayerMaxs.x) * 0.5;
+    point.y = self.m_vecPos.y + (vPlayerMins.y + vPlayerMaxs.y) * 0.5;
+    point.z = self.m_vecPos.z + vPlayerMins.z + 1
 
     iWaterLevel = EWaterLevel.WL_NotInWater;
 
@@ -75,13 +74,13 @@ function GameMovement:CheckWater()
     if (cont & MASK_WATER) ~= 0 then
         iWaterLevel = EWaterLevel.WL_Feet;
 
-        point.z = vTargetOrigin.z + (vPlayerMins.z + vPlayerMaxs.z) * 0.5;
+        point.z = self.m_vecPos.z + (vPlayerMins.z + vPlayerMaxs.z) * 0.5;
         cont = engine.GetPointContents(point, 1);
 
         if (cont & MASK_WATER) ~= 0 then
             iWaterLevel = EWaterLevel.WL_Waist;
 
-            point.z = vTargetOrigin.z + GetViewOffset(self.m_hTarget).z;
+            point.z = self.m_vecPos.z + GetViewOffset(self.m_hTarget).z;
             cont = engine.GetPointContents(point, 2);
             if (cont & MASK_WATER) ~= 0 then
                 iWaterLevel = EWaterLevel.WL_Eyes;
@@ -182,14 +181,14 @@ function GameMovement:SetupSpeed()
         return;
     end
 
+    --[[ does nothing
     local flSpeed = self.m_vecVelocity:Length();
     if flSpeed == 0 then
         return;
     end
 
     flSpeed = math.sqrt(flSpeed);
-
-    local flSpeedFactor = 1.0;
+    ]]
 
     --[[
         Skipping surface data and constraint
@@ -199,17 +198,155 @@ function GameMovement:SetupSpeed()
     self:SpeedCrop();
 end
 
+--- wtf is this?
+function GameMovement:TestPlayerPosition(pos)
+    local trace;
+    local vMins, vMaxs = self.m_hTarget:GetMins(), self.m_hTarget:GetMaxs();
+    trace = engine.TraceHull(pos, pos, vMins, vMaxs, MASK_PLAYERSOLID, function(ent)
+        if ent:IsPlayer() and ent:GetIndex() == self.m_hTarget:GetIndex() then
+            return false;
+        end
+
+        return true;
+    end);
+
+    return trace.entity;
+end
+
 --- why is this so big?!!?!?!?
 function GameMovement:CheckStuck()
-    local base;
+    --[[local base;
     local offset;
-    local test;
+    local test;]]
     local hitent;
-    local idx;
+    --[[local idx;
     local fTime;
+    local trace;]]
+
+    hitent = self:TestPlayerPosition(self.m_vecPos);
+    if hitent == nil then
+        return false;
+    end
+
+    --- there is a bunch of shit here
+    --- but honestly idk wtf they are doing
+
+    return true;
+end
+
+--- i simplified it a bit
+--- as they are only clamping them
+function GameMovement:CheckVelocity()
+    local _, sv_maxvelocity = client.GetConVar("sv_maxvelocity");
+    self.m_vecVelocity.x = clamp(self.m_vecVelocity.x, -sv_maxvelocity, sv_maxvelocity);
+    self.m_vecVelocity.y = clamp(self.m_vecVelocity.y, -sv_maxvelocity, sv_maxvelocity);
+    self.m_vecVelocity.z = clamp(self.m_vecVelocity.z, -sv_maxvelocity, sv_maxvelocity);
+end
+
+function GameMovement:StartGravity()
+    --local ent_gravity = 1.0;
+    local _, sv_gravity = client.GetConVar("sv_gravity");
+    self.m_vecVelocity.z = self.m_vecVelocity.z - (sv_gravity * 0.5 * globals.TickInterval());
+    self.m_vecVelocity.z = self.m_vecVelocity.z + self.m_vecVelocity.z * globals.TickInterval();
+    self:CheckVelocity();
+end
+
+function GameMovement:Friction()
+    -- skip if water jump time is active (not implemented, so skip check)
+
+    local speed = self.m_vecVelocity:Length()
+    if speed < 0.1 then
+        return
+    end
+
+    local drop = 0
+
+    local groundEntity = self.m_hTarget:GetPropEntity("movetype") ~= E_MoveType.MOVETYPE_NONE
+        and self.m_hTarget:GetPropEntity("m_hGroundEntity")
+
+    if groundEntity then
+        local _, sv_friction = client.GetConVar("sv_friction")
+        local surfaceFriction = self.m_hTarget:GetPropFloat("m_flFriction") or 1.0
+        local friction = sv_friction * surfaceFriction
+
+        local _, sv_stopspeed = client.GetConVar("sv_stopspeed")
+        local control = (speed < sv_stopspeed) and sv_stopspeed or speed
+
+        local frameTime = globals.FrameTime()
+        drop = drop + control * friction * frameTime
+    end
+
+    local newspeed = speed - drop
+    if newspeed < 0 then
+        newspeed = 0
+    end
+
+    if newspeed ~= speed then
+        local scale = newspeed / speed
+        self.m_vecVelocity = self.m_vecVelocity * scale
+
+        if self.m_outWishVel then
+            self.m_outWishVel = self.m_outWishVel - ((1.0 - scale) * self.m_vecVelocity)
+        end
+    end
+end
+
+local function CheckGround(vPos, vMins, vMaxs, step_height)
     local trace;
+    trace = engine.TraceHull(vPos, vPos - step_height, vMins, vMaxs, MASK_PLAYERSOLID, function (ent, contentsMask)
+        if ent:IsPlayer() then
+            return false;
+        end
 
+        return true;
+    end)
 
+    return trace and trace.fraction < 1;
+end
+
+function GameMovement:WalkMove2(vWishPos, vWishDir, flWishSpeed)
+    
+end
+
+function GameMovement:AirMove()
+end
+
+function GameMovement:FullWalkMove()
+    if not self:CheckWater() then
+        self:StartGravity();
+    end
+
+    if self.m_iWaterLevel >= EWaterLevel.WL_Waist then
+        --- WaterMove();
+    else
+        local vMins, vMaxs;
+        vMins = self.m_hTarget:GetMins();
+        vMaxs = self.m_hTarget:GetMaxs();
+        local bGround = CheckGround(self.m_vecPos, vMins, vMaxs, self.m_hTarget:GetPropFloat("m_flStepSize"))
+
+        if bGround then
+            self:Friction();
+        end
+
+        self:CheckVelocity();
+
+        if bGround then
+            self:WalkMove2();
+        else
+            self:AirMove();
+        end
+    end
+end
+
+function GameMovement:HandlePlayerMove()
+    local moveType = self.m_hTarget:GetMoveType();
+
+    --- very good ik
+    if moveType == E_MoveType.MOVETYPE_NONE then
+        return;
+    elseif moveType == E_MoveType.MOVETYPE_WALK then
+        self:FullWalkMove();
+    end
 end
 
 return GameMovement
